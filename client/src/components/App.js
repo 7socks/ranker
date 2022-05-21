f8335d8b24183b185fc2a0f6e1ba71baa88a8faf
Elfric <elf72742@gmail.com>
2022-05-21 11:54:33 -0400

    sorting page base

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,26 +2,35 @@ import React from 'react';
 import { parseList } from '../util/index.js';
 
 import ListInput from './ListInput.js';
+import Sorter from './Sorter.js';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      view: 'input'
+      view: 'input',
+      list: []
     };
 
     this.startSort = this.startSort.bind(this);
   }
 
   startSort(list) {
-
+    this.setState({
+      view: 'sort',
+      list: list
+    })
   }
 
   render() {
     if (this.state.view === 'input') {
       return (
         <ListInput ready={this.startSort}/>
+      );
+    } else if (this.state.view === 'sort') {
+      return (
+        <Sorter list={this.state.list}/>
       );
     }
   }
