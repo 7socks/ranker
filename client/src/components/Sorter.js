a6cce0faa59a59bbe9c67262181e0af7a69b4f3d
Elfric <elf72742@gmail.com>
2022-05-22 12:04:27 -0400

    sorting process finishes

--- client/src/components/Sorter.js
+++ client/src/components/Sorter.js
@@ -18,21 +18,32 @@ class Sorter extends React.Component {
   }
 
   select(selection) {
+    console.log(selection);
     let item = this.state[selection];
     let merged = this.state.merged;
     let list = this.state.list;
     merged.push(item);
+    console.log(list);
 
     let block1 = this.state.list[this.state.blocks[0]];
     let block2 = this.state.list[this.state.blocks[1]];
+    if (selection === 'item1') {
+      block1.splice(block1.indexOf(this.state.item1), 1);
+    } else {
+      block2.splice(block1.indexOf(this.state.item2), 1);
+    }
+
+    if (block1.length === 0 && block2.length > 0) {
+      merged = merged.concat(block2);
+      block2 = [];
+    } else if (block2.length === 0 && block1.length > 0) {
+      merged = merged.concat(block1);
+      block1 = [];
+    }
 
     let newState = {};
     if (block1.length + block2.length > 0) {
-      if (selection === 'item1') {
-        block1.splice(block1.indexOf(this.state.item1), 1);
-      } else {
-        block2.splice(block1.indexOf(this.state.item2), 1);
-      }
+      console.log('option 1')
       list.splice(this.state.blocks[0], 2, block1, block2);
       newState = {
         list: list,
@@ -40,19 +51,21 @@ class Sorter extends React.Component {
         item1: block1[0],
         item2: block2[0]
       };
-    } else if (this.state.blocks[1] === this.state.list.length) {
+    } else if (this.state.blocks[1] === this.state.list.length - 1) {
+      console.log('option 2')
       list.splice(this.state.blocks[0], 2, merged);
-      item === this.state.item1 ? merged.push(item2) : merged.push(item1);
+      //item === this.state.item1 ? merged.push(item2) : merged.push(item1);
       newState = {
         list: list,
         merged: [],
         blocks: [0, 1],
-        item1: list[0][0],
-        item2: list[1][0]
+        item1: list[0] ? list[0][0] : null,
+        item2: list[1] ? list[1][0] : null
       };
     } else {
+      console.log('option 3')
       list.splice(this.state.blocks[0], 2, merged);
-      newBlocks = [this.state.blocks[0] + 1, this.state.blocks[1] + 1];
+      let newBlocks = [this.state.blocks[0] + 1, this.state.blocks[1] + 1];
       newState = {
         list: list,
         blocks: newBlocks,
