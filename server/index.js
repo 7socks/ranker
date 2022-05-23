6974515b7f96a4818f4b4fb7b639e6fa21f7e1f2
Elfric <elf72742@gmail.com>
2022-05-23 14:39:35 -0400

    added custom port for heroku

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000...');
+let port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
 });
