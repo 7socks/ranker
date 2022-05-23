5eed123289c858df7f7b828819ecf9ae58fc2e53
Elfric <elf72742@gmail.com>
2022-05-23 14:11:27 -0400

    removed log

--- client/src/util/index.js
+++ client/src/util/index.js
@@ -6,7 +6,6 @@ const parseList = (str) => {
   var items = [];
   var lines = str.split('\n');
 
-  console.log(lines);
   for (var i = 0; i < lines.length; i++) {
     if (lines[i].length > 0) {
       var item = new Item(lines[i]);
