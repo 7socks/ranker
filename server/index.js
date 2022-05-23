b4a428f0863c8dbbaf6920ebfb2e5f15d44d9940
Elfric <elf72742@gmail.com>
2022-05-23 14:40:06 -0400

    semicolon

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-let port = process.env.PORT || 3000
+let port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
